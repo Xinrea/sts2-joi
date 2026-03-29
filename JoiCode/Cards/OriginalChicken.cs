42c1cc1a3a3d39bf2025715b87e704c04c332e5b
Xinrea <xinreas@outlook.com>
2026-03-29 22:49:40 +0800

    feat: add Original Chicken card with localization in English and Chinese

---
+++ JoiCode/Cards/OriginalChicken.cs
@@ -0,0 +1,30 @@
+using BaseLib.Utils;
+using Joi.JoiCode.Character;
+using Joi.JoiCode.Powers;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace Joi.JoiCode.Cards;
+
+[Pool(typeof(JoiCardPool))]
+public class OriginalChicken : JoiCard
+{
+    public OriginalChicken() : base(2, CardType.Skill, CardRarity.Uncommon, TargetType.Self) { }
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var whiteHole = Owner.Creature.GetPower<WhiteHolePower>();
+        if (whiteHole != null && whiteHole.Amount > 0)
+        {
+            await CommonActions.ApplySelf<WhiteHolePower>(this, whiteHole.Amount);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        EnergyCost.UpgradeBy(-1);
+    }
+}
