c31797d2b622305da65eac7d8925cac6b18aa33f
Xinrea <xinreas@outlook.com>
2026-04-01 20:59:03 +0800

    feat: update EnchantmentIconPatch to redirect FeedEnchantment icon to a custom image path for improved visual representation

--- JoiCode/Patches/EnchantmentIconPatch.cs
+++ JoiCode/Patches/EnchantmentIconPatch.cs
@@ -1,4 +1,5 @@
 using HarmonyLib;
+using Joi.JoiCode.Enchantments;
 using MegaCrit.Sts2.Core.Models;
 
 namespace Joi.JoiCode.Patches;
@@ -6,15 +7,14 @@ namespace Joi.JoiCode.Patches;
 [HarmonyPatch(typeof(EnchantmentModel), nameof(EnchantmentModel.IconPath), MethodType.Getter)]
 public static class EnchantmentIconPatch
 {
+    private const string FeedIconPath = "res://Joi/images/enchantments/feed_enchantment.png";
+
     [HarmonyPostfix]
     public static void Postfix(EnchantmentModel __instance, ref string __result)
     {
-        // 如果是 FeedEnchantment 且图标路径是 missing_enchantment，替换为我们的图标
-        if (__instance.GetType().Name == "FeedEnchantment" &&
-            __result.Contains("missing_enchantment"))
+        if (__instance is FeedEnchantment)
         {
-            __result = "res://Joi/images/enchantments/feed_enchantment.png";
-            MainFile.Logger.Info($"[EnchantmentIconPatch] Redirected FeedEnchantment icon to {__result}");
+            __result = FeedIconPath;
         }
     }
 }
