4f6f4232c2228fc1b36897696c23f281534262d7
Xinrea <xinreas@outlook.com>
2026-04-01 19:41:30 +0800

    feat: introduce Only Orange card with mechanics for exhausting hand cards and generating new Orange cards; add localization updates in English and Chinese, and include associated image assets

---
+++ JoiCode/Cards/OnlyOrange.cs
@@ -0,0 +1,58 @@
+using BaseLib.Utils;
+using Joi.JoiCode.Character;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+
+namespace Joi.JoiCode.Cards;
+
+[Pool(typeof(JoiCardPool))]
+public class OnlyOrange : JoiCard
+{
+    public OnlyOrange() : base(2, CardType.Skill, CardRarity.Uncommon, TargetType.Self) { }
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromCard<Orange>(IsUpgraded)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var hand = Owner.PlayerCombatState.Hand;
+        var cardsToExhaust = hand.Cards.Where(c => c != this).ToList();
+        var count = cardsToExhaust.Count;
+
+        // 消耗所有其他手牌
+        foreach (var card in cardsToExhaust)
+        {
+            await CardCmd.Exhaust(choiceContext, card, false, false);
+        }
+
+        if (count == 0)
+        {
+            return;
+        }
+
+        // 生成橘子卡
+        var combatState = Owner.Creature.CombatState;
+        var oranges = new List<CardModel>();
+
+        for (int i = 0; i < count; i++)
+        {
+            var orange = combatState.CreateCard<Orange>(Owner);
+            if (IsUpgraded)
+            {
+                orange.UpgradeInternal();
+            }
+            oranges.Add(orange);
+        }
+
+        await CardPileCmd.AddGeneratedCardsToCombat(oranges, PileType.Hand, true, CardPilePosition.Random);
+    }
+
+    protected override void OnUpgrade()
+    {
+        // Upgrade changes the generated cards to Orange+
+    }
+}
