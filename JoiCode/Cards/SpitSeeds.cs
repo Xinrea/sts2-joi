cd251087dcd864d759d50538320be014abc102b3
Xinrea <xinreas@outlook.com>
2026-04-07 18:59:27 +0800

    feat: add Spit Seeds card with damage mechanics based on orange count in exhaust pile; include localization updates in English and Chinese, and add associated image assets

---
+++ JoiCode/Cards/SpitSeeds.cs
@@ -0,0 +1,42 @@
+using BaseLib.Utils;
+using Joi.JoiCode.Character;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Joi.JoiCode.Cards;
+
+[Pool(typeof(JoiCardPool))]
+public class SpitSeeds : JoiCard
+{
+    public SpitSeeds() : base(1, CardType.Skill, CardRarity.Uncommon, TargetType.AnyEnemy) { }
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DynamicVar("Multiplier", 3)
+    ];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromCard<Orange>(false)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var exhaustPile = CardPile.Get(PileType.Exhaust, Owner);
+        var orangeCount = exhaustPile?.Cards.Count(c => c is Orange) ?? 0;
+
+        var multiplier = (int)DynamicVars["Multiplier"].BaseValue;
+        var damage = orangeCount * multiplier;
+
+        if (damage > 0 && cardPlay.Target != null)
+        {
+            await CreatureCmd.Damage(choiceContext, [cardPlay.Target], damage, ValueProp.Move, Owner.Creature, this);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars["Multiplier"].UpgradeValueBy(2);
+    }
+}
