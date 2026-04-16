a68e79455ec4c0822c7be010541b4635b20e4436
Xinrea <xinreas@outlook.com>
2026-04-16 19:56:06 +0800

    feat: <光子喷射> 调整效果

--- JoiCode/Cards/PhotonJet.cs
+++ JoiCode/Cards/PhotonJet.cs
@@ -1,9 +1,11 @@
 using BaseLib.Utils;
 using Joi.JoiCode.Character;
 using Joi.JoiCode.Powers;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
 
 namespace Joi.JoiCode.Cards;
 
@@ -14,18 +16,28 @@ public class PhotonJet : JoiCard
 
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
-        new CardsVar(1),
-        new DynamicVar("WhiteHole", 1)
+        new DynamicVar("Triggers", 1)
     ];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        await CommonActions.Draw(this, choiceContext);
-        await CommonActions.ApplySelf<WhiteHolePower>(this, DynamicVars["WhiteHole"].BaseValue);
+        var whiteHolePower = Owner.Creature.GetPower<WhiteHolePower>();
+        if (whiteHolePower != null && whiteHolePower.Amount > 0)
+        {
+            var triggers = DynamicVars["Triggers"].IntValue;
+            for (int i = 0; i < triggers; i++)
+            {
+                if (whiteHolePower.Amount > 0)
+                {
+                    await CreatureCmd.Damage(choiceContext, Owner.Creature.CombatState?.Enemies.ToList() ?? [], whiteHolePower.Amount, ValueProp.Unpowered, Owner.Creature);
+                    await PowerCmd.Decrement(whiteHolePower);
+                }
+            }
+        }
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars.Cards.UpgradeValueBy(1);
+        DynamicVars["Triggers"].UpgradeValueBy(1);
     }
 }
