63bb1c058d871933019cc1c42295b514f8a21f0e
Xinrea <xinreas@outlook.com>
2026-04-18 22:47:17 +0800

    fix: <宇宙常数>

--- JoiCode/Powers/CosmicConstantPower.cs
+++ JoiCode/Powers/CosmicConstantPower.cs
@@ -6,6 +6,7 @@ using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
 
 namespace Joi.JoiCode.Powers;
 
@@ -13,6 +14,12 @@ public class CosmicConstantPower : JoiPower, IAfterTurnStart
 {
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.None;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DynamicVar("BlackHole", 1),
+        new DynamicVar("WhiteHole", 1)
+    ];
 
     public async Task AfterTurnStart(TurnContext context)
     {
