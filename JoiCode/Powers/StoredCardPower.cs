95385588be0b7f53e4e4c39a88eb3327591ad9af
Xinrea <xinreas@outlook.com>
2026-04-19 17:28:14 +0800

    fix: 存储特定卡牌无法打出的问题

--- JoiCode/Powers/StoredCardPower.cs
+++ JoiCode/Powers/StoredCardPower.cs
@@ -1,15 +1,10 @@
-using BaseLib.Abstracts;
-using BaseLib.Utils;
 using Godot;
-using Joi.JoiCode.Minions;
-using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Nodes.Cards;
 using MegaCrit.Sts2.Core.Nodes.Combat;
@@ -25,7 +20,6 @@ public class StoredCardPower : JoiPower
 {
     private CardModel? _storedCard;
     private bool _played;
-    private PlayerChoiceContext? _cachedChoiceContext;
 
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Single;
@@ -72,42 +66,36 @@ public class StoredCardPower : JoiPower
         await CardPileCmd.Add(card, PileType.Hand);
     }
 
-    public override Task BeforeSideTurnStart(PlayerChoiceContext choiceContext, CombatSide side, CombatState combatState)
+    public override async Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player)
     {
-        if (side == CombatSide.Player)
-        {
-            _cachedChoiceContext = choiceContext;
-        }
-        return Task.CompletedTask;
-    }
-
-    public override async Task AfterSideTurnStart(CombatSide side, CombatState combatState)
-    {
-        if (side != CombatSide.Player) return;
-        if (_storedCard == null || _played || _cachedChoiceContext == null) return;
+        if (player != Owner.PetOwner) return;
+        if (_storedCard == null || _played) return;
 
         _played = true;
         var card = _storedCard;
         _storedCard = null;
-        var choiceContext = _cachedChoiceContext;
-        _cachedChoiceContext = null;
 
         ClearStoredCardVisual();
 
         if (Owner.IsAlive)
         {
             // 检查玩家是否有 AxisCoreHandsomePower（打出两次）
-            var player = combatState.PlayerCreatures.FirstOrDefault(c => !c.IsPet && c.IsAlive);
-            bool hasAxisCoreHandsome = player?.GetPower<AxisCoreHandsomePower>() != null;
+            bool hasAxisCoreHandsome = player.Creature.GetPower<AxisCoreHandsomePower>() != null;
 
             // 默认打出1次，轴芯好帅打出2次
             int playCount = hasAxisCoreHandsome ? 2 : 1;
 
             MainFile.Logger.Info($"[StoredCardPower] Auto-playing stored card {card.Id} x{playCount}");
 
-            for (int i = 0; i < playCount; i++)
+            var cardsToPlay = new List<CardModel> { card };
+            if (hasAxisCoreHandsome)
             {
-                await CardCmd.AutoPlay(choiceContext, card, null);
+                cardsToPlay.Add(card.CreateDupe());
+            }
+
+            foreach (var cardToPlay in cardsToPlay)
+            {
+                await CardCmd.AutoPlay(choiceContext, cardToPlay, null);
             }
         }
         else
