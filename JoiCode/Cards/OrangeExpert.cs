bd8eead69ef3db1a7c8acfe39461227819d407ca
Xinrea <xinreas@outlook.com>
2026-04-20 19:07:57 +0800

    fix: <橘子专家> 修复效果

--- JoiCode/Cards/OrangeExpert.cs
+++ JoiCode/Cards/OrangeExpert.cs
@@ -1,32 +1,29 @@
 using BaseLib.Utils;
 using Joi.JoiCode.Character;
 using Joi.JoiCode.Powers;
-using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-using MegaCrit.Sts2.Core.ValueProps;
 
 namespace Joi.JoiCode.Cards;
 
 [Pool(typeof(JoiCardPool))]
 public class OrangeExpert : JoiCard
 {
-    public OrangeExpert() : base(3, CardType.Skill, CardRarity.Uncommon, TargetType.Self) { }
+    public OrangeExpert() : base(3, CardType.Power, CardRarity.Uncommon, TargetType.Self) { }
 
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
-        new DamageVar(3, ValueProp.Move),
-        new PowerVar<OrangeExpertPower>(1)
+        new DynamicVar("Damage", 3)
     ];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        await PowerCmd.Apply<OrangeExpertPower>(Owner.Creature, DynamicVars["OrangeExpertPower"].BaseValue, Owner.Creature, this);
+        await CommonActions.ApplySelf<OrangeExpertPower>(this, DynamicVars["Damage"].BaseValue);
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars.Damage.UpgradeValueBy(1);
+        EnergyCost.UpgradeBy(-1);
     }
 }
