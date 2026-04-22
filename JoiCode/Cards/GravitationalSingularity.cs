5bc7cdb7a716f4cbcaf4935bcbaebd7667ac10c6
Xinrea <xinreas@outlook.com>
2026-04-22 21:23:30 +0800

    feat: 重做 引力奇点

--- JoiCode/Cards/GravitationalSingularity.cs
+++ JoiCode/Cards/GravitationalSingularity.cs
@@ -1,9 +1,12 @@
 using BaseLib.Utils;
 using Joi.JoiCode.Character;
 using Joi.JoiCode.Powers;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
 
 namespace Joi.JoiCode.Cards;
 
@@ -14,19 +17,23 @@ public class GravitationalSingularity : JoiCard
 
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
-        new DynamicVar("BlackHole", 3),
-        new DynamicVar("WhiteHole", 3)
+        new DynamicVar("WhiteHole", 2)
     ];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        await CommonActions.ApplySelf<BlackHolePower>(this, DynamicVars["BlackHole"].BaseValue);
+        var blackHole = Owner.Creature.GetPower<BlackHolePower>();
+        if (blackHole != null && blackHole.Amount > 0)
+        {
+            await CreatureCmd.Damage(choiceContext, [Owner.Creature], blackHole.Amount, ValueProp.Unpowered, Owner.Creature, null);
+            await PowerCmd.Remove(blackHole);
+        }
+
         await CommonActions.ApplySelf<WhiteHolePower>(this, DynamicVars["WhiteHole"].BaseValue);
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars["BlackHole"].UpgradeValueBy(2);
-        DynamicVars["WhiteHole"].UpgradeValueBy(2);
+        DynamicVars["WhiteHole"].UpgradeValueBy(1);
     }
 }
